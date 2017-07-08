6b68d88054491e0aab851a39166cb59ef94e7294
Anders Juul <anders_privat@hotmail.com>
2017-07-08 11:07:19 +0200

    a

---
+++ Src/Web/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Serilog;
+
+namespace Web
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Log.Logger = Ajf.Nuget.Logging.StandardLoggerConfigurator.GetEnrichedLogger();
+
+            Log.Logger.Information("Starting...");
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
